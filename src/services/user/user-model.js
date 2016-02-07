b6b85340bb4c5861137a60e2ad0f10c15a78acae
spyros.marinis <spyros.marinis@softwerk.se>
2016-02-07 22:29:29 +0100

    Initial commit

---
+++ src/services/user/user-model.js
@@ -0,0 +1,19 @@
+// user.js - A mongoose model
+//
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  name: {type: String, required: true, index: true},
+  username: {type: String, required: true, unique: true},
+  password: {type: String, required: true},
+  createdAt: {type: Date, 'default': Date.now},
+  updatedAt: {type: Date, 'default': Date.now}
+});
+
+let userModel = mongoose.model('user', userSchema);
+
+export default userModel;
